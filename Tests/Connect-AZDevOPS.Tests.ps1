7ea68ebe501c9298af4becdaca162759158d6cf3
John Roos <john.m.roos@gmail.com>
2022-01-31 22:54:46 +0100

    Changed use of InModuleScope for Connect tests

--- Tests/Connect-AZDevOPS.Tests.ps1
+++ Tests/Connect-AZDevOPS.Tests.ps1
@@ -1,43 +1,57 @@
+#Requires -Module @{ ModuleName = 'Pester'; ModuleVersion = '5.3.1' }
+
 Remove-Module AZDevOPS -Force -ErrorAction SilentlyContinue
 Import-Module $PSScriptRoot\..\Source\AZDevOPS -Force
 
-InModuleScope -ModuleName AZDevOPS {
-    BeforeAll {        
-        Remove-Variable AZDevOPSCredentials -ErrorAction SilentlyContinue
-        
-        $DummyUser = 'DummyUserName'
-        $DummyPassword = 'DummyPassword'
-        $DummyOrg = 'DummyOrg'
-    }
-    
-    Describe 'Creating connection variable' {
-        Context 'mocking stuff' {
-            BeforeAll {
-                Mock -CommandName InvokeAZDevOPSRestMethod -MockWith {} -ModuleName AZDevOPS
-                Connect-AZDevOPS -Username $DummyUser -PersonalAccessToken $DummyPassword -Organization $DummyOrg
-            }
 
-            It 'Should create a AZDevOPSCredentials variable' {
+BeforeAll {
+    InModuleScope -ModuleName 'AZDevOPS' {
+        Remove-Variable AZDevOPSCredentials -Scope 'Script' -ErrorAction SilentlyContinue
+    }
+
+    $DummyUser = 'DummyUserName'
+    $DummyPassword = 'DummyPassword'
+    $DummyOrg = 'DummyOrg'
+}
+
+Describe 'Creating connection variable' {
+    Context 'mocking stuff' {
+        BeforeAll {
+            Mock -CommandName InvokeAZDevOPSRestMethod -MockWith {} -ModuleName AZDevOPS
+            Connect-AZDevOPS -Username $DummyUser -PersonalAccessToken $DummyPassword -Organization $DummyOrg
+        }
+
+        It 'Should create a AZDevOPSCredentials variable' {
+            InModuleScope -ModuleName 'AZDevOPS' {
                 Get-Variable -Name 'AZDevOPSCredentials' -Scope 'Script' | Should -Not -BeNullOrEmpty
             }
-            It 'Variable should be encrypted (PSCredential)' {
+        }
+        It 'Variable should be encrypted (PSCredential)' {
+            InModuleScope -ModuleName 'AZDevOPS' {
                 $Script:AZDevOPSCredentials | Should -BeOfType [pscredential]
             }
-            It 'Username should be set' {
-                $Script:AZDevOPSCredentials.UserName | Should -Be $DummyUser
+        }
+        It 'Username should be set' {
+            InModuleScope -ModuleName 'AZDevOPS' -Parameters @{'DummyUser' = $DummyUser} {
+               $Script:AZDevOPSCredentials.UserName | Should -Be $DummyUser
             }
-            It 'Password should be set' {
+        }
+        It 'Password should be set' {
+            InModuleScope -ModuleName 'AZDevOPS' -Parameters @{'DummyPassword' = $DummyPassword} {
                 $Script:AZDevOPSCredentials.GetNetworkCredential().Password| Should -Be $DummyPassword
             }
-            It 'AzDoOrganization should be set' {
+        }
+        It 'AzDoOrganization should be set' {
+            InModuleScope -ModuleName 'AZDevOPS' -Parameters @{'DummyOrg' = $DummyOrg} {
                 $Script:AzDOOrganization | Should -Be $DummyOrg
             }
-            It 'Should have called mock' {
-                Should -Invoke InvokeAZDevOPSRestMethod -Exactly 1 -Scope Context -ModuleName AZDevOPS
-            }
+        }
+        It 'Should have called mock' {
+            Should -Invoke InvokeAZDevOPSRestMethod -Exactly 1 -Scope Context -ModuleName AZDevOPS
         }
     }
 }
+
 
 Describe 'Verifying parameters' {
     BeforeAll {
