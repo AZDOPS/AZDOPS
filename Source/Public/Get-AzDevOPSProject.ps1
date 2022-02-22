3fc5c416a34b135ec9ac5fe42f7e897bf25e0c0a
Emanuel Palm <ep__palm@hotmail.com>
2022-02-22 17:27:02 +0100

    Organization parameter not mandatory on Get-AzDevOPSProject

--- Source/Public/Get-AzDevOPSProject.ps1
+++ Source/Public/Get-AzDevOPSProject.ps1
@@ -1,7 +1,7 @@
 function Get-AzDevOPSProject {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory)]
+        [Parameter()]
         [string]$Organization,
 
         [Parameter()]
@@ -13,15 +13,17 @@ function Get-AzDevOPSProject {
     }
     else {
         $Org = GetAZDevOPSHeader
+        $Organization = $Org['Organization']
     }
 
-    $Uri = "https://dev.azure.com/$($Org['Organization'])/_apis/projects?api-version=7.1-preview.4"
+    $Uri = "https://dev.azure.com/$Organization/_apis/projects?api-version=7.1-preview.4"
 
     $Method = 'GET'
-    $ProjectInfo = (InvokeAZDevOPSRestMethod -Uri $Uri -Method $Method -Organization $Org['Organization']).value 
-    if($ProjectName) {
+    $ProjectInfo = (InvokeAZDevOPSRestMethod -Uri $Uri -Method $Method -Organization $Organization).value
+
+    if (-not [string]::IsNullOrWhiteSpace($ProjectName)) {
         $ProjectInfo = $ProjectInfo | Where-Object -Property Name -eq $ProjectName
     }
 
-    $ProjectInfo
+    Write-Output $ProjectInfo
 }
