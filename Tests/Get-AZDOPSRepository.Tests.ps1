e5a77142052e2d7160ace073fa8657edc6a0339d
John Roos <JohnRoos@users.noreply.github.com>
2022-02-22 21:59:39 +0100

    Added Get-AZDOPSRepository
    Co-Authored-By: Robin Hardstål <75914904+Hardstl@users.noreply.github.com>
    Co-Authored-By: Sebastian Claesson <34689432+SebastianClaesson@users.noreply.github.com>

---
+++ Tests/Get-AZDOPSRepository.Tests.ps1
@@ -0,0 +1,54 @@
+Remove-Module AZDOPS -ErrorAction SilentlyContinue
+Import-Module $PSScriptRoot\..\Source\AZDOPS
+
+Describe "Get-AZDOPSRepository" {
+    Context "Function tests" {
+        It "Function exists" {
+            { Get-Command -Name Get-AZDOPSRepository -Module AZDOPS -ErrorAction Stop } | Should -Not -Throw
+        }
+
+        It 'Has parameter <_>' -TestCases 'Organization', 'Project', 'Repository' {
+            (Get-Command -Name Get-AZDOPSRepository).Parameters.Keys | Should -Contain $_
+        }
+    }
+
+    Context "Function returns repositories" {
+        BeforeAll {
+            Mock InvokeAZDOPSRestMethod -ModuleName AZDOPS {
+                [PSCustomObject]@{
+                    Value = @(
+                        @{
+                            Id = "84eba821-52d5-4ba8-a50b-63640ce234b8"
+                        },
+                        @{
+                            Id = "250c42f9-f55e-4c64-a11c-d0d83a4f7fcc"
+                        }
+                    )
+                }
+            }
+        }
+
+        It "Returns repositories" {
+            Get-AZDOPSRepository -Organization 'MyOrg' -Project 'MyProject' | Should -Not -BeNullOrEmpty
+        }
+
+        It 'Returns an id' {
+            (Get-AZDOPSRepository -Organization 'MyOrg' -Project 'MyProject').id | Should -Contain '84eba821-52d5-4ba8-a50b-63640ce234b8'
+        }
+
+        It 'Calls InvokeAZDOPSRestMethod with correct parameters when repository is used' {
+            Get-AZDOPSRepository -Organization 'MyOrg' -Project 'MyProject' -Repository 'MyRepo'
+            Should -Invoke InvokeAZDOPSRestMethod -ModuleName AZDOPS -Times 1 -Exactly -ParameterFilter {$Uri -eq 'https://dev.azure.com/MyOrg/MyProject/_apis/git/repositories/MyRepo?api-version=7.1-preview.1'}
+        }
+
+        It 'Can handle single repository responses from API' {
+            Mock InvokeAZDOPSRestMethod -ModuleName AZDOPS {
+                [PSCustomObject]@{
+                    Name = 'Fisar'
+                }
+            }
+            
+            (Get-AZDOPSRepository -Organization 'MyOrg' -Project 'MyProject' -Repository 'MyRepo').Name | Should -Be 'Fisar'
+        }
+    }
+}
