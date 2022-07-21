0387086cc2f9bb1c50fcf9c782f10cd9b1451aab
krokofant <krokofant@users.noreply.github.com>
2022-07-21 18:43:34 +0200

    ✨ Get-ADOPSUser: List all users

--- Source/Public/Get-ADOPSUser.ps1
+++ Source/Public/Get-ADOPSUser.ps1
@@ -1,12 +1,14 @@
 function Get-ADOPSUser {
-    [CmdletBinding(DefaultParameterSetName = 'Name')]
+    [CmdletBinding(DefaultParameterSetName = 'Default')]
     param (
         [Parameter(Mandatory, ParameterSetName = 'Name', Position = 0)]
         [string]$Name,
         [Parameter(Mandatory, ParameterSetName = 'Descriptor', Position = 0)]
         [string]$Descriptor,
         [Parameter()]
-        [string]$Organization
+        [string]$Organization,
+        [Parameter(ParameterSetName = 'Default', DontShow)]
+        [string]$ContinuationToken
     )
 
     if (-not [string]::IsNullOrEmpty($Organization)) {
@@ -17,12 +19,31 @@ function Get-ADOPSUser {
         $Organization = $Org['Organization']
     }
 
-    if($PSCmdlet.ParameterSetName -eq 'Name') {
+    if ($PSCmdlet.ParameterSetName -eq 'Default') {
+        $Uri = "https://vssps.dev.azure.com/$Organization/_apis/graph/users?api-version=6.0-preview.1"
+        $Method = 'GET'
+        if(-not [string]::IsNullOrEmpty($ContinuationToken)) {
+            $Uri += "&continuationToken=$ContinuationToken"
+        }
+        $Response = (InvokeADOPSRestMethod -FullResponse -Uri $Uri -Method $Method -Organization $Organization)
+        $Users = [System.Collections.ArrayList]::new(1000)
+        $Users.AddRange($Response.Content.value)
+        Write-Verbose "Found $($Response.Content.count) users"
+        if($Response.Headers.ContainsKey('X-MS-ContinuationToken')) {
+            Write-Verbose "Found continuationToken. Will fetch more users."
+            $parameters = [hashtable]$PSBoundParameters
+            $parameters.Add('ContinuationToken', "$($Response.Headers['X-MS-ContinuationToken'])")
+            $Users.AddRange((Get-ADOPSUser @parameters))
+        }
+        Write-Output $Users
+    }
+    elseif ($PSCmdlet.ParameterSetName -eq 'Name') {
         $Uri = "https://vsaex.dev.azure.com/$Organization/_apis/UserEntitlements?`$filter=name eq '$Name'&`$orderBy=name Ascending&api-version=6.0-preview.3"
         $Method = 'GET'
         $Users = (InvokeADOPSRestMethod -Uri $Uri -Method $Method -Organization $Organization).members.user
         Write-Output $Users
-    } elseif ($PSCmdlet.ParameterSetName -eq 'Descriptor') {
+    }
+    elseif ($PSCmdlet.ParameterSetName -eq 'Descriptor') {
         $Uri = "https://vssps.dev.azure.com/$Organization/_apis/graph/users/$Descriptor`?api-version=6.0-preview.1"
         $Method = 'GET'
         $User = (InvokeADOPSRestMethod -Uri $Uri -Method $Method -Organization $Organization)
