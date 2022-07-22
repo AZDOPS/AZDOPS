c60fd2eb2713c6183573d2e245fd9aa504939315
Björn Sundling <bjorn.sundling@advania.com>
2022-07-22 16:45:10 +0200

    Added New-ADOPSWiki

---
+++ Source/Public/New-ADOPSWiki.ps1
@@ -0,0 +1,53 @@
+function New-ADOPSWiki {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string]$Organization,
+
+        [Parameter(Mandatory)]
+        [string]$Project,
+
+        [Parameter(Mandatory)]
+        [string]$WikiName,
+        
+        [Parameter(Mandatory)]
+        [string]$WikiRepository,
+
+        [Parameter()]
+        [string]$WikiRepositoryPath = '/',
+        
+        [Parameter()]
+        [string]$GitBranch = 'main'
+    )
+
+    if (-not [string]::IsNullOrEmpty($Organization)) {
+        $Org = GetADOPSHeader -Organization $Organization
+    }
+    else {
+        $Org = GetADOPSHeader
+        $Organization = $Org['Organization']
+    }
+
+    $ProjectId = (Get-ADOPSProject -Project $Project).id
+    $RepositoryId = (Get-ADOPSRepository -Project $Project -Repository $WikiRepository).id
+
+    $URI = "https://dev.azure.com/$Organization/_apis/wiki/wikis?api-version=7.1-preview.2"
+    
+    $Method = 'Post'
+    $Body = [ordered]@{
+        'type' = 'codeWiki'
+        'name' = $WikiName
+        'projectId' = $ProjectId
+        'repositoryId' = $RepositoryId
+        'mappedPath' = $WikiRepositoryPath
+        'version' = @{'version' = $GitBranch}
+    } 
+
+    $InvokeSplat = @{
+        Uri = $URI
+        Method = $Method
+        Body = $Body | ConvertTo-Json -Compress
+    }
+
+    InvokeADOPSRestMethod @InvokeSplat
+}
