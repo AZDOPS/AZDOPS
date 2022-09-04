9818a6adcfa56d031c744fa23e91f2a60bdfa1b1
Björn Sundling <bjorn.sundling@advania.com>
2022-09-04 21:53:02 +0200

    fixed tests for New-ADOPSElasticPoolObject

--- Tests/New-ADOPSElasticPoolObject.Tests.ps1
+++ Tests/New-ADOPSElasticPoolObject.Tests.ps1
@@ -74,22 +74,32 @@ Describe "New-ADOPSElasticPoolObject" {
     }
 
     Context "Function returns created elastic pool" {
+        BeforeEach {
+            $Splat = @{
+                ServiceEndpointId = '44868479-e856-42bf-9a2b-74bb500d8e36' 
+                ServiceEndpointScope = 'a36adc0c-a513-4acd-85bf-2c2a7bb62d30' 
+                AzureId = '/subscriptions/6713962a-bebb-45c2-97cd-fb0dead95acf/resourceGroups/resourceGroupName/providers/Microsoft.Compute/virtualMachineScaleSets/vmss-test'
+            }
+        }
 
         It "Returns an output" {
-            New-ADOPSElasticPoolObject -ServiceEndpointId '44868479-e856-42bf-9a2b-74bb500d8e36' -ServiceEndpointScope 'a36adc0c-a513-4acd-85bf-2c2a7bb62d30' -AzureId '/subscriptions/6713962a-bebb-45c2-97cd-fb0dead95acf/resourceGroups/resourceGroupName/providers/Microsoft.Compute/virtualMachineScaleSets/vmss-test' | Should -Not -BeNullOrEmpty
+            New-ADOPSElasticPoolObject @splat  | Should -Not -BeNullOrEmpty
         }
 
         It "Returns a PowerShell object" {
-            (New-ADOPSElasticPoolObject -ServiceEndpointId '44868479-e856-42bf-9a2b-74bb500d8e36' -ServiceEndpointScope 'a36adc0c-a513-4acd-85bf-2c2a7bb62d30' -AzureId '/subscriptions/6713962a-bebb-45c2-97cd-fb0dead95acf/resourceGroups/resourceGroupName/providers/Microsoft.Compute/virtualMachineScaleSets/vmss-test' -OutputType 'pscustomobject').GetType().FullName | Should -Be 'System.Management.Automation.PSCustomObject'
+            (New-ADOPSElasticPoolObject @splat -OutputType 'pscustomobject').GetType().FullName | Should -Be 'System.Management.Automation.PSCustomObject'
         }
 
         It "Returns the given AzureId" {
-            (New-ADOPSElasticPoolObject -ServiceEndpointId '44868479-e856-42bf-9a2b-74bb500d8e36' -ServiceEndpointScope 'a36adc0c-a513-4acd-85bf-2c2a7bb62d30' -AzureId '/subscriptions/6713962a-bebb-45c2-97cd-fb0dead95acf/resourceGroups/resourceGroupName/providers/Microsoft.Compute/virtualMachineScaleSets/vmss-test').azureId | Should -Be '/subscriptions/6713962a-bebb-45c2-97cd-fb0dead95acf/resourceGroups/resourceGroupName/providers/Microsoft.Compute/virtualMachineScaleSets/vmss-test'
+            (New-ADOPSElasticPoolObject @splat).azureId | Should -Be '/subscriptions/6713962a-bebb-45c2-97cd-fb0dead95acf/resourceGroups/resourceGroupName/providers/Microsoft.Compute/virtualMachineScaleSets/vmss-test'
         }
 
         It "Returns a Json object/string" {
-            (New-ADOPSElasticPoolObject -ServiceEndpointId '44868479-e856-42bf-9a2b-74bb500d8e36' -ServiceEndpointScope 'a36adc0c-a513-4acd-85bf-2c2a7bb62d30' -AzureId '/subscriptions/6713962a-bebb-45c2-97cd-fb0dead95acf/resourceGroups/resourceGroupName/providers/Microsoft.Compute/virtualMachineScaleSets/vmss-test' -OutputType 'json').GetType().FullName | Should -Be 'System.String'
+            (New-ADOPSElasticPoolObject @splat -OutputType 'json').GetType().FullName | Should -Be 'System.String'
         }
 
+        It "DesiredIdle needs to be less than MaxCapacity" {
+            {New-ADOPSElasticPoolObject -DesiredIdle 2 -MaxCapacity 1  @splat} | Should -Throw
+        }
     }
 }
