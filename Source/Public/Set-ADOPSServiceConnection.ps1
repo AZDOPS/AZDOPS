cd2928932d2e6a3a1cd1ad24bb354f59ef55f16d
sebastian claesson <sebastian.claesson@advania.se>
2022-09-10 09:45:47 +0200

    Adding new function and documentation

---
+++ Source/Public/Set-ADOPSServiceConnection.ps1
@@ -0,0 +1,129 @@
+function Set-ADOPSServiceConnection {
+    [CmdletBinding(DefaultParameterSetName = 'ServicePrincipal')]
+    param (
+        [Parameter()]
+        [string]$Organization,
+        
+        [Parameter(Mandatory)]
+        [string]$TenantId,
+
+        [Parameter(Mandatory)]
+        [string]$SubscriptionName,
+
+        [Parameter(Mandatory)]
+        [string]$SubscriptionId,
+
+        [Parameter(Mandatory)]
+        [string]$Project,
+
+        [Parameter(Mandatory)]
+        [guid]$ServiceEndpointId,
+
+        [Parameter()]
+        [string]$ConnectionName,
+
+        [Parameter()]
+        [string]$Description,
+        
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [string]$EndpointOperation,
+      
+        [Parameter(Mandatory, ParameterSetName = 'ServicePrincipal')]
+        [pscredential]$ServicePrincipal,
+
+        [Parameter(Mandatory, ParameterSetName = 'ManagedServiceIdentity')]
+        [switch]$ManagedIdentity
+    )
+    
+    process {
+
+        # Set organization
+        if (-not [string]::IsNullOrEmpty($Organization)) {
+            $Org = GetADOPSHeader -Organization $Organization
+        }
+        else {
+            $Org = GetADOPSHeader
+            $Organization = $Org['Organization']
+        }
+
+        # Get ProjectId
+        $ProjectInfo = Get-ADOPSProject -Organization $Organization -Project $Project
+
+        # Set connection name if not set by parameter
+        if (-not $ConnectionName) {
+            $ConnectionName = $SubscriptionName -replace " "
+        }
+
+        switch ($PSCmdlet.ParameterSetName) {
+        
+            'ServicePrincipal' {
+                $authorization = [ordered]@{
+                    parameters = [ordered]@{
+                        tenantid            = $TenantId
+                        serviceprincipalid  = $ServicePrincipal.UserName
+                        authenticationType  = "spnKey"
+                        serviceprincipalkey = $ServicePrincipal.GetNetworkCredential().Password
+                    }
+                    scheme     = "ServicePrincipal"
+                }
+        
+                $data = [ordered]@{
+                    subscriptionId   = $SubscriptionId
+                    subscriptionName = $SubscriptionName
+                    environment      = "AzureCloud"
+                    scopeLevel       = "Subscription"
+                    creationMode     = "Manual"
+                }
+            }
+    
+            'ManagedServiceIdentity' {
+                $authorization = [ordered]@{
+                    parameters = [ordered]@{
+                        tenantid = $TenantId
+                    }
+                    scheme     = "ManagedServiceIdentity"
+                }
+            }
+        }
+
+        # Create body for the API call
+        $Body = [ordered]@{
+            authorization                    = $authorization
+            data                             = $data
+            description                      = "$Description"
+            id                               = $ServiceConnectionId
+            isReady                          = $true
+            isShared                         = $false
+            name                             = ($SubscriptionName -replace " ")
+            serviceEndpointProjectReferences = @(
+                [ordered]@{
+                    projectReference = [ordered]@{
+                        id   = $ProjectInfo.Id
+                        name = $Project
+                    }
+                    name             = $ConnectionName
+                }
+            )
+            type                             = "AzureRM"
+            url                              = "https://management.azure.com/"
+        } | ConvertTo-Json -Depth 10
+    
+        if ($PSBoundParameters.ContainsKey('EndpointOperation')) {
+            $URI = "https://dev.azure.com/$Organization/_apis/serviceendpoint/endpoints/$ServiceEndpointId`?operation=$EndpointOperation`&api-version=7.1-preview.4"
+        }
+        else {
+            $URI = "https://dev.azure.com/$Organization/_apis/serviceendpoint/endpoints/$ServiceEndpointId`?api-version=7.1-preview.4"
+        }
+        
+        $InvokeSplat = @{
+            Uri          = $URI
+            Method       = "PUT"
+            Body         = $Body
+            Organization = $Organization
+        }
+    
+        InvokeADOPSRestMethod @InvokeSplat
+
+    }
+}
