83de6a61f2fddae29d7bcdee3ea06d14aa3d11ec
Björn Sundling <bjorn.sundling@gmail.com>
2023-03-29 10:31:38 +0200

    New-ADOPSGitBranch

---
+++ Source/Public/New-ADOPSGitBranch.ps1
@@ -0,0 +1,49 @@
+function New-ADOPSGitBranch {
+    param (
+    [Parameter()]
+    [ValidateNotNullOrEmpty()]
+    [string]$Organization,
+
+    [Parameter(Mandatory)]
+    [ValidatePattern('^[a-z0-9]{8}\-[a-z0-9]{4}\-[a-z0-9]{4}\-[a-z0-9]{4}\-[a-z0-9]{12}$', ErrorMessage = 'RepositoryId must be in GUID format (xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx)')]
+    [string]$RepositoryId,
+
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string]$Project,
+
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string]$BranchName,
+
+    [Parameter(Mandatory)]
+    [ValidateLength(40,40)]
+    [string]$CommitId
+    )
+    
+    if (-not [string]::IsNullOrEmpty($Organization)) {
+        $Org = GetADOPSHeader -Organization $Organization
+    }
+    else {
+        $Org = GetADOPSHeader
+        $Organization = $Org['Organization']
+    }
+
+    $Body = @(
+        [ordered]@{
+            name = "refs/heads/$BranchName"
+            oldObjectId = '0000000000000000000000000000000000000000'
+            newObjectId = $CommitId
+        }
+    )
+    $Body = ConvertTo-Json -InputObject $Body -Compress
+
+    $Uri = "https://dev.azure.com/$Organization/$Project/_apis/git/repositories/$RepositoryId/refs?api-version=7.1-preview.1"
+    $InvokeSplat = @{
+        Uri = $Uri
+        Method = 'Post'
+        Body = $Body
+    }
+
+    InvokeADOPSRestMethod @InvokeSplat
+}
---
+++ Tests/New-ADOPSGitBranch.Tests.ps1
@@ -0,0 +1,107 @@
+param(
+    $PSM1 = "$PSScriptRoot\..\Source\ADOPS.psm1"
+)
+
+BeforeAll {
+    Remove-Module ADOPS -Force -ErrorAction SilentlyContinue
+    Import-Module $PSM1 -Force
+}
+
+Describe 'New-ADOPSGitBranch' {
+    Context 'Parameters' {
+        $TestCases = @(
+            @{
+                Name = 'Organization'
+                Mandatory = $false
+                Type = 'string'
+            },
+            @{
+                Name = 'Project'
+                Mandatory = $true
+                Type = 'string'
+            },
+            @{
+                Name = 'RepositoryId'
+                Mandatory = $true
+                Type = 'string'
+            },
+            @{
+                Name = 'BranchName'
+                Mandatory = $true
+                Type = 'string'
+            },
+            @{
+                Name = 'CommitId'
+                Mandatory = $true
+                Type = 'string'
+            }
+        )
+    
+        It 'Should have parameter <_.Name>' -TestCases $TestCases  {
+            Get-Command New-ADOPSGitBranch | Should -HaveParameter $_.Name -Mandatory:$_.Mandatory -Type $_.Type
+        }
+    }
+
+    
+    Context "Functionality" {
+        BeforeAll {
+            InModuleScope -ModuleName ADOPS {
+                Mock -CommandName GetADOPSHeader -ModuleName ADOPS -MockWith {
+                    @{
+                        Header       = @{
+                            'Authorization' = 'Basic Base64=='
+                        }
+                        Organization = $Organization
+                    }
+                } -ParameterFilter { $Organization }
+                Mock -CommandName GetADOPSHeader -ModuleName ADOPS -MockWith {
+                    @{
+                        Header       = @{
+                            'Authorization' = 'Basic Base64=='
+                        }
+                        Organization = 'DummyOrg'
+                    }
+                }
+                
+                Mock -CommandName InvokeADOPSRestMethod  -ModuleName ADOPS -MockWith {
+                    return $InvokeSplat
+                }
+            }
+        }
+
+        It 'If organization is given, in should call GetADOPSHeader with organization name' {
+            $r = New-ADOPSGitBranch -Organization 'DummyOrg' -Project 'DummyProj' -RepositoryId 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa' -BranchName 'myNewBranch' -CommitId 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+            Should -Invoke -CommandName GetADOPSHeader -ModuleName ADOPS -ParameterFilter { $Organization }
+        }
+        It 'If organization is not given, in should call GetADOPSHeader with no parameters' {
+            $r = New-ADOPSGitBranch -Project 'DummyProj' -RepositoryId 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa' -BranchName 'myNewBranch' -CommitId 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+            Should -Invoke -CommandName GetADOPSHeader -ModuleName ADOPS
+        }
+
+        It 'Creates correct URI' {
+            $required = "https://dev.azure.com/DummyOrg/DummyProj/_apis/git/repositories/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa/refs?api-version=7.1-preview.1"
+            $actual = New-ADOPSGitBranch -Project 'DummyProj' -RepositoryId 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa' -BranchName 'myNewBranch' -CommitId 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+            $actual.Uri | Should -Be $required
+        }
+
+        It 'Method sould be Post' {
+            $required = 'Post'
+            $actual = New-ADOPSGitBranch -Project 'DummyProj' -RepositoryId 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa' -BranchName 'myNewBranch' -CommitId 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+            $actual.Method | Should -Be $required
+        }
+
+        It 'Verifying body' {
+            $required = '[{"name":"refs/heads/myNewBranch","oldObjectId":"0000000000000000000000000000000000000000","newObjectId":"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"}]'
+            $actual = New-ADOPSGitBranch -Project 'DummyProj' -RepositoryId 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa' -BranchName 'myNewBranch' -CommitId 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+            $actual.Body | Should -Be $required
+        }
+
+        It 'If CommitId is to short, it should throw' {
+            {New-ADOPSGitBranch -Project 'DummyProj' -RepositoryId 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa' -BranchName 'myNewBranch' -CommitId 'aaaaaaa'} | Should -Throw -Because 'We need the full commit ID hash, not just the short version'
+        }
+
+        It 'RepoID must be GUID style' {
+            {New-ADOPSGitBranch -Project 'DummyProj' -RepositoryId 'NotAGuid' -BranchName 'myNewBranch' -CommitId 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'} | Should -Throw
+        }
+    }
+}
