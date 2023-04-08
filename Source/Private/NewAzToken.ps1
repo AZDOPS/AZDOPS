0af4f0566d4ec9e90e2beda8c75ed9b74f00d741
Björn Sundling <bjorn.sundling@gmail.com>
2023-04-08 23:08:20 +0200

    remove organization needed in NewAzToken

--- Source/Private/NewAzToken.ps1
+++ Source/Private/NewAzToken.ps1
@@ -1,12 +1,10 @@
 function NewAzToken {
-    param (
-        [string]$ShouldBeDefault
-    )
+    param ()
 
     $t = Get-AzToken -Resource 499b84ac-1321-427f-aa17-267ca6975798 
 
     # Get User context
-    $me = Invoke-RestMethod -Method GET 'https://vssps.dev.azure.com/bjornsundling/_apis/profile/profiles/me?api-version=7.1-preview.3' -Headers @{
+    $me = Invoke-RestMethod -Method GET 'https://app.vssps.visualstudio.com/_apis/profile/profiles/me?api-version=7.1-preview.3' -Headers @{
         Authorization = "Bearer $($t.token)"
     }
 
@@ -22,7 +20,7 @@ function NewAzToken {
             Organization = $organization
             OauthToken = $t
             UserContext = $me
-            Default = [bool]($ShouldBeDefault -eq $organization)
+            Default = $false
         }
         $res += $tokenData
     }
