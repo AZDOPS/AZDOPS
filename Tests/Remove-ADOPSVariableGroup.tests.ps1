f977c22eee0c2da954516f7220f51defe5afa43c
Emanuel Palm <emanuel+github@pipe.how>
2023-05-11 09:54:11 +0200

    Remove-ADOPSVariableGroup tests

--- Tests/Remove-ADOPSVariableGroup.tests.ps1
+++ Tests/Remove-ADOPSVariableGroup.tests.ps1
@@ -34,22 +34,7 @@ Describe 'Remove-ADOPSVariableGroup' {
 
     Context 'Removing variable group' {
         BeforeAll {
-            Mock -CommandName GetADOPSHeader -ModuleName ADOPS -MockWith {
-                @{
-                    Header       = @{
-                        'Authorization' = 'Basic Base64=='
-                    }
-                    Organization = $OrganizationName
-                }
-            } -ParameterFilter { $OrganizationName -eq 'Organization' }
-            Mock -CommandName GetADOPSHeader -ModuleName ADOPS -MockWith {
-                @{
-                    Header       = @{
-                        'Authorization' = 'Basic Base64=='
-                    }
-                    Organization = $OrganizationName
-                }
-            }
+            Mock -CommandName GetADOPSDefaultOrganization -ModuleName ADOPS -MockWith { 'DummyOrg' }
 
             Mock -CommandName InvokeADOPSRestMethod -ModuleName ADOPS -MockWith {
                 return ''
