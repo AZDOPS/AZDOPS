8cd05a6f2c982c4006c64374b339b2cfb35566a7
Emanuel Palm <emanuel+github@pipe.how>
2023-05-11 11:26:06 +0200

    Get-ADOPSNode tests

--- Source/Public/Get-ADOPSNode.ps1
+++ Source/Public/Get-ADOPSNode.ps1
@@ -16,7 +16,7 @@ function Get-ADOPSNode {
     $Uri = "https://dev.azure.com/$Organization/_apis/distributedtask/elasticpools/$PoolId/nodes?api-version=7.1-preview.1"
 
     $Method = 'GET'
-    $NodeInfo = InvokeADOPSRestMethod -Uri $Uri -Method $Method -Organization $Organization
+    $NodeInfo = InvokeADOPSRestMethod -Uri $Uri -Method $Method
 
     if ($NodeInfo.psobject.properties.name -contains 'value') {
         Write-Output $NodeInfo.value
