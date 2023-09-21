88808eb2595d5493c601499849900ee099f32d1f
Björn Sundling <bjorn.sundling@advania.com>
2023-09-21 09:12:25 +0200

    add workload identity federation support (#192)

--- Tests/New-ADOPSServiceConnection.Tests.ps1
+++ Tests/New-ADOPSServiceConnection.Tests.ps1
@@ -49,6 +49,16 @@ Describe 'New-ADOPSServiceConnection' {
                 Name = 'ManagedIdentity'
                 Mandatory = $true
                 Type = 'switch'
+            },
+            @{
+                Name = 'WorkloadIdentityFederation'
+                Mandatory = $true
+                Type = 'switch'
+            },
+            @{
+                Name = 'AzureScope'
+                Mandatory = $true
+                Type = 'string'
             }
         )
     
@@ -77,30 +87,49 @@ Describe 'New-ADOPSServiceConnection' {
                 TenantId = 'AzureTennantId' 
                 SubscriptionName = 'AzureSubscriptionName' 
                 SubscriptionId = 'AzureSubscriptionId' 
-                ServicePrincipal = [pscredential]::new('User', (ConvertTo-SecureString -String 'PassWord' -AsPlainText -Force))
             }
         }
 
         It 'Should not get organization from GetADOPSDefaultOrganization when organization parameter is used' {
+            $Splat.Add('ServicePrincipal', [pscredential]::new('User', (ConvertTo-SecureString -String 'PassWord' -AsPlainText -Force)))
             New-ADOPSServiceConnection -Organization 'anotherorg' @Splat
             Should -Invoke GetADOPSDefaultOrganization -ModuleName ADOPS -Times 0 -Exactly
         }
 
         It 'Should get organization using GetADOPSDefaultOrganization when organization parameter is not used' {
+            $Splat.Add('ServicePrincipal', [pscredential]::new('User', (ConvertTo-SecureString -String 'PassWord' -AsPlainText -Force)))
             New-ADOPSServiceConnection @Splat
             Should -Invoke GetADOPSDefaultOrganization -ModuleName ADOPS -Times 1 -Exactly
         }
 
         It 'Verifying URI is correct' {
+            $Splat.Add('ServicePrincipal', [pscredential]::new('User', (ConvertTo-SecureString -String 'PassWord' -AsPlainText -Force)))
             Mock -CommandName InvokeADOPSRestMethod -ModuleName ADOPS -MockWith { return $URI}
             $r = New-ADOPSServiceConnection @Splat
-            $r | Should -Be 'https://dev.azure.com/myorg/myproj/_apis/serviceendpoint/endpoints?api-version=7.1-preview.4'
+            $r | Should -Be 'https://dev.azure.com/myorg/myproj/_apis/serviceendpoint/endpoints?api-version=7.2-preview.4'
         }
 
-        It 'Verifying body is correct' {
+        It 'Verifying body is correct - ServicePrincipal' {
+            $Splat.Add('ServicePrincipal', [pscredential]::new('User', (ConvertTo-SecureString -String 'PassWord' -AsPlainText -Force)))
             Mock -CommandName InvokeADOPSRestMethod -ModuleName ADOPS -MockWith { return $body}
             $r = New-ADOPSServiceConnection @Splat | ConvertFrom-Json | ConvertTo-Json -Compress -Depth 10
             $r | Should -Be '{"data":{"subscriptionId":"AzureSubscriptionId","subscriptionName":"AzureSubscriptionName","environment":"AzureCloud","scopeLevel":"Subscription","creationMode":"Manual"},"name":"AzureSubscriptionName","type":"AzureRM","url":"https://management.azure.com/","authorization":{"parameters":{"tenantid":"AzureTennantId","serviceprincipalid":"User","authenticationType":"spnKey","serviceprincipalkey":"PassWord"},"scheme":"ServicePrincipal"},"isShared":false,"isReady":true,"serviceEndpointProjectReferences":[{"projectReference":{"id":"ProjectInfoId","name":"myproj"},"name":"AzureSubscriptionName"}]}'
+        }
+
+        It 'Verifying body is correct - ManagedServiceIdentity' {
+            $Splat.Add('ServicePrincipal', [pscredential]::new('User', (ConvertTo-SecureString -String 'PassWord' -AsPlainText -Force)))
+            $Splat.Add('ManagedIdentity', $true)
+            Mock -CommandName InvokeADOPSRestMethod -ModuleName ADOPS -MockWith { return $body}
+            $r = New-ADOPSServiceConnection @Splat | ConvertFrom-Json | ConvertTo-Json -Compress -Depth 10
+            $r | Should -Be '{"data":{"subscriptionId":"AzureSubscriptionId","subscriptionName":"AzureSubscriptionName","environment":"AzureCloud","scopeLevel":"Subscription"},"name":"AzureSubscriptionName","type":"AzureRM","url":"https://management.azure.com/","authorization":{"parameters":{"tenantid":"AzureTennantId","serviceprincipalid":"User","serviceprincipalkey":"PassWord"},"scheme":"ManagedServiceIdentity"},"isShared":false,"isReady":true,"serviceEndpointProjectReferences":[{"projectReference":{"id":"ProjectInfoId","name":"myproj"},"name":"AzureSubscriptionName"}]}'
+        }
+
+        It 'Verifying body is correct - WorkloadIdentityFederation' {
+            $Splat.Add('WorkloadIdentityFederation', $true)
+            $Splat.Add('AzureScope', 'Azure/Scope')
+            Mock -CommandName InvokeADOPSRestMethod -ModuleName ADOPS -MockWith { return $body}
+            $r = New-ADOPSServiceConnection @Splat | ConvertFrom-Json | ConvertTo-Json -Compress -Depth 10
+            $r | Should -Be '{"data":{"subscriptionId":"AzureSubscriptionId","subscriptionName":"AzureSubscriptionName","environment":"AzureCloud","scopeLevel":"Subscription","creationMode":"Automatic"},"name":"AzureSubscriptionName","type":"AzureRM","url":"https://management.azure.com/","authorization":{"parameters":{"tenantid":"AzureTennantId","scope":"Azure/Scope"},"scheme":"WorkloadIdentityFederation"},"isShared":false,"isReady":true,"serviceEndpointProjectReferences":[{"projectReference":{"id":"ProjectInfoId","name":"myproj"},"name":"AzureSubscriptionName"}]}'
         }
     }
 }
