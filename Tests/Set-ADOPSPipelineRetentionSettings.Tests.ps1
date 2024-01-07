9a3032947e9b547b96ad29cb36a2311cfc238bc1
coolhome <700740+coolhome@users.noreply.github.com>
2024-01-07 15:54:52 -0500

    Cleaned up test

--- Tests/Set-ADOPSPipelineRetentionSettings.Tests.ps1
+++ Tests/Set-ADOPSPipelineRetentionSettings.Tests.ps1
@@ -32,79 +32,81 @@ Describe 'Set-ADOPSPipelineRetentionSettings' {
         }
     }
 
-    Context 'Getting Pipeline Retention Settings' {
+    Context 'Setting Pipeline Retention Settings' {
         BeforeAll {
             $OrganizationName = 'DummyOrg'
             $Project = 'DummyProject'
             $Setting = 'artifactsRetention'
 
-#             Mock -CommandName InvokeADOPSRestMethod -ModuleName ADOPS -MockWith {
-#                 return @'
-#                 {
-#                     "purgeArtifacts": 43,
-#                     "purgePullRequestRuns": 2,
-#                     "purgeRuns": 30,
-#                     "retainRunsPerProtectedBranch": null
-#                 }
-# '@ | ConvertFrom-Json
-#             } -ParameterFilter { $Method -eq 'Patch' -and $Uri -like '*_apis/build/retention*' }
-
-            Mock -CommandName GetADOPSDefaultOrganization -ModuleName ADOPS -MockWith { 'DummyOrg' }
-        }
-
-        It 'uses InvokeADOPSRestMethod single times' {
-            Set-ADOPSPipelineRetentionSettings -Organization $OrganizationName -Project $Project -Values @{ $Setting = 43 }
-            Should -Invoke 'InvokeADOPSRestMethod' -ModuleName 'ADOPS' -Exactly -Times 1
-        }
-        It 'should not throw with all parameters' {
-            { Set-ADOPSPipelineRetentionSettings -Organization $OrganizationName -Project $Project -Values @{ $Setting = 43 } } | Should -Not -Throw
-        }
-        It 'should not throw without Organization parameter' {
-            { Set-ADOPSPipelineRetentionSettings -Project $Project -Values @{ artifactsRetention = 43 } } | Should -Not -Throw
-        }
-        It 'returns outputs after patching pipeline settings' {
-            (Set-ADOPSPipelineRetentionSettings -Project $Project -Values @{ artifactsRetention = 43 } | Get-Member -MemberType NoteProperty).count | Should -Be 4
-        }
-        It 'should convert values into correct API format' {
-
             Mock -CommandName InvokeADOPSRestMethod -ModuleName ADOPS -MockWith {
                 return @'
                 {
-                    "purgeArtifacts": 43,
-                    "purgePullRequestRuns": 2,
-                    "purgeRuns": 30,
+                    "purgeArtifacts": {
+                        "min": 1,
+                        "max": 60,
+                        "value": 40
+                    },
+                    "purgePullRequestRuns": {
+                        "min": 1,
+                        "max": 30,
+                        "value": 2
+                    },
+                    "purgeRuns": {
+                        "min": 30,
+                        "max": 731,
+                        "value": 30
+                    },
                     "retainRunsPerProtectedBranch": null
                 }
 '@ | ConvertFrom-Json
             } -ParameterFilter { $Method -eq 'Patch' -and $Uri -like '*_apis/build/retention*' }
 
-            Set-ADOPSPipelineRetentionSettings -Organization $OrganizationName -Project $Project -Values @{ 
+            Mock -CommandName GetADOPSDefaultOrganization -ModuleName ADOPS -MockWith { 'DummyOrg' }
+        }
+
+        It 'uses InvokeADOPSRestMethod single times' {
+            $Values = @{ 
+                $Setting = 43 
+            }
+            Set-ADOPSPipelineRetentionSettings -Organization $OrganizationName -Project $Project -Values $Values
+            Should -Invoke 'InvokeADOPSRestMethod' -ModuleName 'ADOPS' -Exactly -Times 1
+        }
+        
+        It 'should not throw with all parameters' {
+            $Values = @{ 
+                $Setting = 43 
+            }
+            { Set-ADOPSPipelineRetentionSettings -Organization $OrganizationName -Project $Project -Values $Values } | Should -Not -Throw
+        }
+        
+        It 'should not throw without Organization parameter' {
+            $Values = @{
+                artifactsRetention = 43
+            }
+            { Set-ADOPSPipelineRetentionSettings -Project $Project -Values $Values } | Should -Not -Throw
+        }
+        
+        It 'returns outputs after patching pipeline settings' {
+            $Values = @{ 
+                artifactsRetention = 43 
+            }
+            (Set-ADOPSPipelineRetentionSettings -Project $Project -Values $Values | Get-Member -MemberType NoteProperty).count | Should -Be 4
+        }
+
+        It 'should convert response type ProjectRetentionSetting into UpdateProjectRetentionSettingModel property names' {
+            $Values = @{ 
                 artifactsRetention           = 40
                 runRetention                 = 30
                 pullRequestRunRetention      = 2
                 retainRunsPerProtectedBranch = 1
             }
-            Should -Invoke 'InvokeADOPSRestMethod' -ModuleName ADOPS -ParameterFilter { 
-                $ParsedBody = $Body | ConvertFrom-Json -Depth 3
-                
-                $ParsedBody.artifactsRetention.min | Should -BeNullOrEmpty
-                $ParsedBody.artifactsRetention.max | Should -BeNullOrEmpty
-                $ParsedBody.artifactsRetention.value | Should -BeExactly 40
- 
-                $ParsedBody.runRetention.min | Should -BeNullOrEmpty
-                $ParsedBody.runRetention.max | Should -BeNullOrEmpty
-                $ParsedBody.runRetention.value | Should -BeExactly 30
 
-                $ParsedBody.pullRequestRunRetention.min | Should -BeNullOrEmpty
-                $ParsedBody.pullRequestRunRetention.max | Should -BeNullOrEmpty
-                $ParsedBody.pullRequestRunRetention.value | Should -BeExactly 2
-                
-                $ParsedBody.retainRunsPerProtectedBranch.min | Should -BeNullOrEmpty
-                $ParsedBody.retainRunsPerProtectedBranch.max | Should -BeNullOrEmpty
-                $ParsedBody.retainRunsPerProtectedBranch.value | Should -BeExactly 1
-
-                $true
-            } -Times 1 -Exactly
+            $Response = Set-ADOPSPipelineRetentionSettings -Organization $OrganizationName -Project $Project -Values $Values
+            
+            $Response.artifactsRetention | Should -BeExactly 40
+            $Response.runRetention | Should -BeExactly 30
+            $Response.pullRequestRunRetention | Should -BeExactly 2
+            $Response.retainRunsPerProtectedBranch | Should -BeNullOrEmpty
         }
     }
 }
