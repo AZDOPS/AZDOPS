b141213a712b97e31e222a704dd496c5aad79485
Björn Sundling <bjorn.sundling@gmail.com>
2024-03-27 21:13:58 +0100

    Get-ADOPSArtifactFeed

---
+++ Source/Public/Get-ADOPSArtifactFeed.ps1
@@ -0,0 +1,48 @@
+function Get-ADOPSArtifactFeed {
+    [CmdletBinding(DefaultParameterSetName = 'All')]
+    param (
+        [Parameter(ParameterSetName = 'All')]
+        [Parameter(ParameterSetName = 'FeedId', Mandatory)]
+        [string]$Project,
+        
+        [Parameter(ParameterSetName = 'All')]
+        [Parameter(ParameterSetName = 'FeedId')]
+        [string]$Organization,
+
+        [Parameter(ParameterSetName = 'FeedId', Mandatory)]
+        [string]$FeedId
+    )
+    
+    # If user didn't specify org, get it from saved context
+    if ([string]::IsNullOrEmpty($Organization)) {
+        $Organization = GetADOPSDefaultOrganization
+    }
+    
+    $Uri = "https://feeds.dev.azure.com/${Organization}"
+    if (-not ([string]::IsNullOrEmpty($Project))) {
+        $Uri = "${Uri}/${Project}"
+    }
+    $Uri = "${Uri}/_apis/packaging/feeds"
+        if (-not ([string]::IsNullOrEmpty($FeedId))) {
+        $Uri = "${Uri}/${FeedId}"
+    }
+    $Uri = "${Uri}?api-version=7.2-preview.1"
+    
+    $Method = 'Get'
+
+    $InvokeSplat = @{
+        Uri = $Uri
+        Method = $Method
+    }
+
+    $res = InvokeADOPSRestMethod @InvokeSplat
+    if ( 
+        (($res | Get-Member -MemberType NoteProperty).Name -contains 'count') -and 
+        (($res | Get-Member -MemberType NoteProperty).Name -contains 'value')
+    ) {
+        Write-Output $res.value -NoEnumerate
+    }
+    else {
+        Write-Output $res
+    }
+}
