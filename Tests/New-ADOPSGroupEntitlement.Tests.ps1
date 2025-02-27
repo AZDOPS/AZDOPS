f5818f1cb797289e553848436a4ff53aa8585768
Robin <hardstal@hotmail.com>
2025-02-27 19:48:36 +0100

    removed unnecessary tests and added one for body

--- Tests/New-ADOPSGroupEntitlement.Tests.ps1
+++ Tests/New-ADOPSGroupEntitlement.Tests.ps1
@@ -71,30 +71,6 @@ Describe 'New-ADOPSGroupEntitlement' {
             Should -Invoke 'InvokeADOPSRestMethod' -ModuleName 'ADOPS' -Exactly -Times 1
         }
 
-        It 'should throw with missing GroupOriginId parameter' {
-            { New-ADOPSGroupEntitlement -ProjectId $testProjectId -AccountLicenseType 'Express' -ProjectGroupType 'projectContributor' } | Should -Throw
-        }
-
-        It 'should throw with missing AccountLicenseType parameter' {
-            { New-ADOPSGroupEntitlement -GroupOriginId $testGroupId -ProjectId $testProjectId -ProjectGroupType 'projectContributor' } | Should -Throw
-        }
-
-        It 'should throw with missing ProjectGroupType parameter' {
-            { New-ADOPSGroupEntitlement -GroupOriginId $testGroupId -ProjectId $testProjectId -AccountLicenseType 'Express' } | Should -Throw
-        }
-
-        It 'should throw with missing ProjectId parameter' {
-            { New-ADOPSGroupEntitlement -GroupOriginId $testGroupId -AccountLicenseType 'Express' -ProjectGroupType 'projectContributor' } | Should -Throw
-        }
-
-        It 'should throw with invalid AccountLicenseType parameter' {
-            { New-ADOPSGroupEntitlement -GroupOriginId $testGroupId -ProjectId $testProjectId -AccountLicenseType 'Invalid' -ProjectGroupType 'projectContributor' } | Should -Throw
-        }
-
-        It 'should throw with invalid ProjectGroupType parameter' {
-            { New-ADOPSGroupEntitlement -GroupOriginId $testGroupId -ProjectId $testProjectId -AccountLicenseType 'Express' -ProjectGroupType 'Invalid' } | Should -Throw
-        }
-
         It 'should not throw with all mandatory parameters' {
             { New-ADOPSGroupEntitlement -GroupOriginId $testGroupId -ProjectId $testProjectId -AccountLicenseType 'Express' -ProjectGroupType 'projectContributor' } | Should -Not -Throw
         }
@@ -106,6 +82,15 @@ Describe 'New-ADOPSGroupEntitlement' {
 
             $result = New-ADOPSGroupEntitlement -Organization $testOrgName -GroupOriginId $testGroupId -ProjectId $testProjectId -AccountLicenseType 'Express' -ProjectGroupType 'projectContributor'
             $result.OriginalString | Should -Be "https://vsaex.dev.azure.com/$testOrgName/_apis/GroupEntitlements?api-version=7.1"
+        }
+
+        It 'Verify body' {
+            Mock -CommandName InvokeADOPSRestMethod -ModuleName ADOPS -MockWith {
+                return $body
+            } -ParameterFilter { $Method -eq 'Post' }
+
+            $result = New-ADOPSGroupEntitlement -Organization $testOrgName -GroupOriginId $testGroupId -ProjectId $testProjectId -AccountLicenseType 'Express' -ProjectGroupType 'projectContributor'
+            $result | Should -Be '{"extensionRules":[{"id":"ms.feed"}],"group":{"origin":"aad","originId":"01d0472d-9949-421e-81d8-fcb5668a394d","subjectKind":"group"},"licenseRule":{"licensingSource":"account","accountLicenseType":"Express"},"projectEntitlements":[{"group":{"groupType":"projectContributor"},"projectRef":{"id":"8130f18e-f65b-431d-a777-5d4a6f3468ba"}}]}'
         }
     }
 }
