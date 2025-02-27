bb5af4b8ef664eb6acf70bbe400249ce03b6e101
Robin Hardstål <75914904+Hardstl@users.noreply.github.com>
2025-02-27 22:12:07 +0100

    New cmd new adops group entitlement (#242)
    * added cmd to create new group entitlements
    
    * added helper file
    
    * change verb from add to new
    
    * fix test "Verify uri"
    
    * removed unnecessary tests and added one for body
    
    * change "verify body" test
    
    * Removed LicensingSource param
    
    * Also removed from test file

---
+++ Source/Public/New-ADOPSGroupEntitlement.ps1
@@ -0,0 +1,80 @@
+function New-ADOPSGroupEntitlement {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$GroupOriginId,
+
+        [Parameter(Mandatory)]
+        [ValidateSet('Express', 'Advanced', 'Stakeholder', 'Professional', 'EarlyAdopter')]
+        [string]$AccountLicenseType,
+
+        [Parameter(Mandatory)]
+        [ValidateSet('projectReader', 'projectContributor', 'projectAdministrator', 'projectStakeholder')]
+        [string]$ProjectGroupType,
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$ProjectId,
+
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [string]$Organization,
+
+        [Parameter()]
+        [switch]$Wait
+    )
+
+    # If user didn't specify org, get it from saved context
+    if ([string]::IsNullOrEmpty($Organization)) {
+        $Organization = GetADOPSDefaultOrganization
+    }
+
+    # Group entitlements endpoint
+    $URI = "https://vsaex.dev.azure.com/$Organization/_apis/GroupEntitlements?api-version=7.1"
+
+    # Initialize the request body
+    $Body = @{
+        extensionRules = @(
+            @{
+                id = 'ms.feed'
+            }
+        )
+        group = @{
+            origin = 'aad'
+            originId = $GroupOriginId
+            subjectKind = 'group'
+        }
+        licenseRule = @{
+            licensingSource = 'account'
+            accountLicenseType = $AccountLicenseType
+        }
+        projectEntitlements = @(
+            @{
+                group = @{
+                    groupType = $ProjectGroupType
+                }
+                projectRef = @{
+                    id = $ProjectId
+                }
+            }
+        )
+    }
+
+    $InvokeSplat = @{
+        Method = 'Post'
+        Uri = $URI
+        Body = ($Body | ConvertTo-Json -Compress -Depth 10)
+    }
+
+    $Out = InvokeADOPSRestMethod @InvokeSplat
+
+    if ($PSBoundParameters.ContainsKey('Wait')) {
+        while ($Out.operationResult.status -eq 'inProgress') {
+            Start-Sleep -Seconds 1
+            $Out = Invoke-ADOPSRestMethod -Uri $Out.operationResult.statusUrl -Method Get
+        }
+    }
+
+    $Out
+}
---
+++ Tests/New-ADOPSGroupEntitlement.Tests.ps1
@@ -0,0 +1,97 @@
+param(
+    $PSM1 = "$PSScriptRoot\..\Source\ADOPS.psm1"
+)
+
+BeforeAll {
+    Remove-Module ADOPS -Force -ErrorAction SilentlyContinue
+    Import-Module $PSM1 -Force
+}
+
+Describe 'New-ADOPSGroupEntitlement' {
+    Context 'Parameters' {
+        $TestCases = @(
+            @{
+                Name = 'GroupOriginId'
+                Mandatory = $true
+                Type = 'string'
+            },
+            @{
+                Name = 'AccountLicenseType'
+                Mandatory = $true
+                Type = 'string'
+            },
+            @{
+                Name = 'ProjectGroupType'
+                Mandatory = $true
+                Type = 'string'
+            },
+            @{
+                Name = 'ProjectId'
+                Mandatory = $true
+                Type = 'string'
+            },
+            @{
+                Name = 'Organization'
+                Mandatory = $false
+                Type = 'string'
+            },
+            @{
+                Name = 'Wait'
+                Mandatory = $false
+                Type = 'switch'
+            }
+        )
+    
+        It 'Should have parameter <_.Name>' -TestCases $TestCases {
+            Get-Command New-ADOPSGroupEntitlement | Should -HaveParameter $_.Name -Mandatory:$_.Mandatory -Type $_.Type
+        }
+    }
+
+    Context 'Adding group entitlement' {
+        BeforeEach {
+            Mock -CommandName GetADOPSDefaultOrganization -ModuleName ADOPS -MockWith { 'myorg' }
+            Mock -CommandName InvokeADOPSRestMethod -ModuleName ADOPS -MockWith {
+                return $InvokeSplat
+            } -ParameterFilter { $Method -eq 'Post' }
+        }
+
+        BeforeAll {
+            $script:testOrgName = 'DummyOrg'
+            $script:testGroupId = '01d0472d-9949-421e-81d8-fcb5668a394d'
+            $script:testProjectId = '8130f18e-f65b-431d-a777-5d4a6f3468ba'
+        }
+
+        It 'uses InvokeADOPSRestMethod once without wait parameter' {
+            New-ADOPSGroupEntitlement -Organization $testOrgName -GroupOriginId $testGroupId -ProjectId $testProjectId -AccountLicenseType 'Express' -ProjectGroupType 'projectContributor'
+            Should -Invoke 'InvokeADOPSRestMethod' -ModuleName 'ADOPS' -Exactly -Times 1
+        }
+
+        It 'should not throw with all mandatory parameters' {
+            { New-ADOPSGroupEntitlement -GroupOriginId $testGroupId -ProjectId $testProjectId -AccountLicenseType 'Express' -ProjectGroupType 'projectContributor' } | Should -Not -Throw
+        }
+
+        It 'Verify uri' {
+            Mock -CommandName InvokeADOPSRestMethod -ModuleName ADOPS -MockWith {
+                return $uri
+            } -ParameterFilter { $Method -eq 'Post' }
+
+            $result = New-ADOPSGroupEntitlement -Organization $testOrgName -GroupOriginId $testGroupId -ProjectId $testProjectId -AccountLicenseType 'Express' -ProjectGroupType 'projectContributor'
+            $result.OriginalString | Should -Be "https://vsaex.dev.azure.com/$testOrgName/_apis/GroupEntitlements?api-version=7.1"
+        }
+
+        It 'Verify body' {
+            Mock -CommandName InvokeADOPSRestMethod -ModuleName ADOPS -MockWith {
+                return $body
+            } -ParameterFilter { $Method -eq 'Post' }
+
+            $result = New-ADOPSGroupEntitlement -Organization $testOrgName -GroupOriginId $testGroupId -ProjectId $testProjectId -AccountLicenseType 'Express' -ProjectGroupType 'projectContributor'
+            $resultObj = $result | ConvertFrom-Json
+            $resultObj.group.origin | Should -Be 'aad'
+            $resultObj.group.originId | Should -Be $testGroupId
+            $resultObj.group.subjectKind | Should -Be 'group'
+            $resultObj.licenseRule.accountLicenseType | Should -Be 'Express'
+            $resultObj.projectEntitlements[0].group.groupType | Should -Be 'projectContributor'
+            $resultObj.projectEntitlements[0].projectRef.id | Should -Be $testProjectId
+        }
+    }
+}
