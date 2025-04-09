fd419ddc11af962a60dc797d4dff210027e582fd
Sebastian Claesson <sebastian.claesson@fora.se>
2025-04-09 22:35:09 +0200

    Adding new function to list admin overview

---
+++ Docs/Help/Get-ADOPSOrganizationAdminOverview.md
@@ -0,0 +1,61 @@
+---
+external help file:
+Module Name:
+online version:
+schema: 2.0.0
+---
+
+# Get-ADOPSOrganizationAdminOverview
+
+## SYNOPSIS
+Get the organization admin overview data.
+
+## SYNTAX
+
+```
+Get-ADOPSOrganizationAdminOverview [[-Organization] <String>]
+ [<CommonParameters>]
+```
+
+## DESCRIPTION
+Get the organization admin overview data.
+
+## EXAMPLES
+
+### Example 1
+```powershell
+PS C:\> Get-ADOPSOrganizationAdminOverview
+```
+
+Get the organization admin overview data.
+
+## PARAMETERS
+
+### -Organization
+The identifier of the Azure DevOps organization.
+
+```yaml
+Type: String
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: 0
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### CommonParameters
+This cmdlet supports the common parameters: -Debug, -ErrorAction, -ErrorVariable, -InformationAction, -InformationVariable, -OutVariable, -OutBuffer, -PipelineVariable, -Verbose, -WarningAction, and -WarningVariable. For more information, see [about_CommonParameters](http://go.microsoft.com/fwlink/?LinkID=113216).
+
+## INPUTS
+
+### None
+
+## OUTPUTS
+
+### System.Object
+## NOTES
+
+## RELATED LINKS
---
+++ Tests/Get-ADOPSOrganizationAdminOverview.Tests.ps1
@@ -0,0 +1,60 @@
+param(
+    $PSM1 = "$PSScriptRoot\..\Source\ADOPS.psm1"
+)
+
+BeforeAll {
+    Remove-Module ADOPS -Force -ErrorAction SilentlyContinue
+    Import-Module $PSM1 -Force
+}
+
+Describe 'Get-ADOPSOrganizationAdminOverview' {
+    Context 'Parameters' {
+        $TestCases = @(
+            @{
+                Name      = 'Organization'
+                Mandatory = $false
+                Type      = 'string'
+            }
+        )
+    
+        It 'Should have parameter <_.Name>' -TestCases $TestCases {
+            Get-Command Get-ADOPSOrganizationAdminOverview | Should -HaveParameter $_.Name -Mandatory:$_.Mandatory -Type $_.Type
+        }
+    }
+    
+    Context 'functionality' {
+        BeforeAll {
+            Mock -CommandName GetADOPSDefaultOrganization -ModuleName ADOPS -MockWith { 'DummyOrg' }
+
+            Mock -CommandName InvokeADOPSRestMethod -ModuleName ADOPS -MockWith {
+                return @'
+                {
+                    "dataProviders": {
+                        "ms.vss-web.component-data": {},
+                        "ms.vss-web.shared-data": null,
+                        "ms.vss-admin-web.organization-admin-overview-delay-load-data-provider": {
+                            "hasDeletePermissions": true,
+                            "hasModifyPermissions": true,
+                            "currentOwner": {
+                                "name": "Organization Owner",
+                                "id": "a5971806-86aa-44a3-a9c9-2f52180f1570",
+                                "email": "Org.Owner@contoso.com"
+                            },
+                            "currentUserId": "07ebeea2-722f-6821-8844-a5f5783e568b",
+                            "organizationTakeover": {},
+                            "showDomainMigration": true,
+                            "disableDomainMigration": false,
+                            "devOpsDomainUrls": true,
+                            "targetDomainUrl": "https://organizationName.visualstudio.com/"
+                        }
+                    }
+                }             
+'@ | ConvertFrom-Json
+            }
+        }
+
+        It 'Should return something' {
+            Get-ADOPSOrganizationAdminOverview | Should -Not -BeNullOrEmpty
+        }
+    }
+}
