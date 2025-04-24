34a4030179f88e3657bdc8a91d1f400bb2ad35bf
Sebastian Claesson <sebastian.claesson@fora.se>
2025-04-24 22:00:51 +0200

    Adding logic to find members in descriptor and membership for descriptor

--- Source/Public/Get-ADOPSMembership.ps1
+++ Source/Public/Get-ADOPSMembership.ps1
@@ -9,7 +9,7 @@ function Get-ADOPSMembership {
         # The default value for direction is 'up' meaning return all memberships where the subject is a member (e.g. all groups the subject is a member of). Alternatively, passing the direction as 'down' will return all memberships where the subject is a container (e.g. all members of the subject group).
         [Parameter()]
         [string]
-        [ValidateSet('up','down')]
+        [ValidateSet('up', 'down')]
         $Direction = 'up'
     )
     
@@ -21,7 +21,16 @@ function Get-ADOPSMembership {
     $Uri = "https://vssps.dev.azure.com/$Organization/_apis/graph/Memberships/$Descriptor`?direction=$Direction&depth=1&api-version=7.2-preview.1"
     $Response = InvokeADOPSRestMethod -Uri $Uri -Method 'GET'
 
-    $Response.value.memberDescriptor | ForEach-Object -begin {
+    switch ($Direction) {
+        'up' {
+            $Membership = $Response.value.containerDescriptor
+        }
+        'down' {
+            $Membership = $Response.value.memberDescriptor
+        }
+    }
+
+    $Membership | ForEach-Object -begin {
         $Members = New-object System.Collections.ArrayList
     } -process {
         $MemberType = ($_).split('.')[0]
