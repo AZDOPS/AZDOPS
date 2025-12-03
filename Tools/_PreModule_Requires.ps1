c1fb74e91c2ad299720e0f68ad9fa8c449a214d3
Sebastian Claesson <sebastian.claesson@fora.se>
2025-12-03 09:30:46 +0100

    Adding functionality for insecure apis and warning

--- Tools/_PreModule_Requires.ps1
+++ Tools/_PreModule_Requires.ps1
@@ -3,3 +3,15 @@
 $script:AzTokenCache = 'adops.cache'
 
 $script:loginMethod = 'Default'
+
+if ($AllowInsecureApis -or $AdopsAllowInsecureApis) {
+    [bool]$script:runInsecureApis = $true
+}
+
+$script:InsecureApisWarning = @'
+This function uses unsupported APIs, that is not loaded per default.
+To run this command anyway, use the -Force flag.
+To load this command on module import, Either
+- Run 'Import-Module .\Source\ADOPS.psm1 -ArgumentList $true'
+- Set '$AdopsAllowInsecureApis = $true' in console before importing the module.
+'@
